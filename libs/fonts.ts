8b13724cc961ce67683f8077c809e58f5e950ad1
Jomel <jomelbatan6@gmail.com>
2026-09-09 15:23:20 +0800

    initial commit

---
+++ libs/fonts.ts
@@ -0,0 +1,18 @@
+import { Yuyu, Kalam } from "next/font/google";
+
+export const yuyu = Yuyu({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-yuyu",
+});
+
+export const kalam = Kalam({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-kalam",
+});
+export const kalamBold = Kalam({
+  subsets: ["latin"],
+  weight: "700",
+  variable: "--font-kalam-bold",
+});
